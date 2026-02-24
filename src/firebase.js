3c46670a4957e4e302987441f817bfd25c45d9e7
nontawat <nonmano2005@gmail.com>
2026-02-25 02:15:08 +0700

    firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,3 +6,10 @@ const firebaseConfig = {
     messagingSenderId: "998792659773",
     appId: "1:998792659773:web:f05d0c54389d69ac13fb54"
 };
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const facebookProvider = new FacebookAuthProvider();
