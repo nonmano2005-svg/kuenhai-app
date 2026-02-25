0ed82663c639773334a9f8b26828b099e73f118a
nontawat <nonmano2005@gmail.com>
2026-02-25 09:33:45 +0700

    add export auth back

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCBQqFykI6mUe2s2NBf3wRE6D5HB4tQg7c",
+    apiKey: "AIzaSyCBJqFykI6mUe2s2NBf3wRE6DSHB4tQg7c",
     authDomain: "kuenhai-v2.firebaseapp.com",
     projectId: "kuenhai-v2",
     storageBucket: "kuenhai-v2.firebasestorage.app",
     messagingSenderId: "965953909852",
-    appId: "1:965953909852:web:30daee4300d6ba93472299",
-    measurementId: "G-202SRCF2W4"
+    appId: "1:965953909852:web:30daee4308d6ba93472299",
+    measurementId: "G-202SRCFZW4"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const facebookProvider = new FacebookAuthProvider(); 
