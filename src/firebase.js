8e908e0ac8062384bd93b163df9c4f9e72c86924
nontawat <nonmano2005@gmail.com>
2026-02-25 10:14:51 +0700

    add quotes back to config

--- src/firebase.js
+++ src/firebase.js
@@ -15,4 +15,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
-export const facebookProvider = new FacebookAuthProvider(); 
+export const facebookProvider = new FacebookAuthProvider();
